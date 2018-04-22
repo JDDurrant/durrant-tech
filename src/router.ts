88fa210a11cbdf7276eea1acc32605356b3f2892
Jack Durrant <jack@durrant.tech>
2018-04-23 00:40:43 +0100

    Started refactoring for multi-app structure

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,33 @@
 import * as express from 'express';
 
-import Controller from './controllers/controller';
-import PageController from './controllers/page.controller';
-import UserController from './controllers/user.controller';
+import API from './apis/api';
+import ArticleModel from './apis/models/article.model';
+import PageModel from './apis/models/page.model';
+import UserModel from './apis/models/user.model';
+
+// import Controller from './controllers/controller';
+// import PageController from './controllers/page.controller';
+// import UserController from './controllers/user.controller';
 
 const router: express.Router = express.Router();
 
 // GET
-router.get('*', Controller.init);
+// router.get('*', Controller.init);
 
-router.get('/', PageController.homepage, Controller.render);
+router.get('/', API.test.bind(API));
 
-// router.get('/blog');
+router.get('/blog', ArticleModel.find.bind(ArticleModel));
 // router.get('/blog/:category');
-// router.get('/blog/:category/:entry');
+// router.get('/blog/:category/:slug');
 
-router.get('/users', UserController.list, Controller.render);
-router.get('/users/add', UserController.form, Controller.render);
-router.get('/users/:user', UserController.view, Controller.render);
+router.get('/users', UserModel.find.bind(UserModel));
+// router.get('/users/add', UserController.form, Controller.render);
+// router.get('/users/:slug', UserController.view, Controller.render);
 
 // At the bottom so it doesn't interfere with other routes
-router.get('/:page', PageController.read, Controller.render);
+// router.get('/:slug', PageController.read, Controller.render);
 
 // POST
-router.post('/users/add', UserController.save, UserController.then, UserController.catch);
+// router.post('/users/add', UserController.save, UserController.then, UserController.catch);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
 import * as logger from 'morgan';
 
-import app from './app';
+import router from './router';
 
-const host: string = process.env.HOST || '127.0.0.1';
-const port: number = parseInt(process.env.PORT) || 3000;
+const api = express();
 
-if(process.env.ENV == "development") {
-    app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
-}
+// Middleware
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, host, () => {
+if(process.env.ENVIRONMENT == 'development')
+	api.use(logger(':method :url :status :res[content-length] - :response-time ms'));
+
+api.use(express.static('./public'));
+api.use(router);
+
+// Server
+const host: string = process.env.API_URL || '127.0.0.1';
+const port: number = parseInt(process.env.API_PORT) || 3000;
+
+api.listen(port, host, () => {
 	console.log('Listening on %s:%d', host, port);
 });
+
+export default api;
